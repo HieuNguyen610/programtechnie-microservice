bc25256194635a6e8e20f32e76e146acf57f1ebf
hieunm2 <hieunm2@vnext.vn>
2025-04-16 09:54:46 +0700

    init product

--- product-service/src/main/java/hieu/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/hieu/productservice/ProductServiceApplication.java
@@ -1,13 +1,13 @@
-package hieu.programtechiemicroservice;
+package hieu.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgramtechieMicroserviceApplication {
+public class ProductServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProgramtechieMicroserviceApplication.class, args);
+        SpringApplication.run(ProductServiceApplication.class, args);
     }
 
 }
--- product-service/src/test/java/hieu/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/hieu/productservice/ProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package hieu.programtechiemicroservice;
+package hieu.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProgramtechieMicroserviceApplicationTests {
+class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {

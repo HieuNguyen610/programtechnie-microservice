74b792bcb998788ae36c17e9a4364c1324ffe5cf
hieunm2 <hieunm2@vnext.vn>
2025-04-16 09:51:14 +0700

    init

---
+++ src/main/java/hieu/programtechiemicroservice/ProgramtechieMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package hieu.programtechiemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramtechieMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramtechieMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/programtechiemicroservice/ProgramtechieMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.programtechiemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramtechieMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
